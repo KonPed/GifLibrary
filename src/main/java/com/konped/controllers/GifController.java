d61c125412e43d66ec0093f088e8abd670758262
kpaidarakis <konstantinos.paidarakis@intrasoft-intl.com>
2018-12-21 00:13:47 +0200

    Add gif object to gif-details view.

--- src/main/java/com/konped/controllers/GifController.java
+++ src/main/java/com/konped/controllers/GifController.java
@@ -1,8 +1,12 @@
 package com.konped.controllers;
 
+import com.konped.model.Gif;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.time.LocalDate;
 
 @Controller
 public class GifController {
@@ -12,10 +16,12 @@ public class GifController {
     return "home";
   }
 
-  @RequestMapping("/gif")
-  @ResponseBody
-  public String gif() {
-    return "A single Gif.";
+  @RequestMapping(value = "/gif", method = RequestMethod.GET)
+  public String gifDetails(ModelMap modelMap) {
+    Gif gif = new Gif("compiler-bot", LocalDate.of(1987, 4, 28),
+            "konstantinos", true );
+    modelMap.addAttribute(gif);
+    return "gif-details";
   }
 
 }
