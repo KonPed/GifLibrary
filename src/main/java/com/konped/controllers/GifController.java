a499eaa0e0302647f37e82774baa9065cf84a6d0
kpaidarakis <konstantinos.paidarakis@intrasoft-intl.com>
2019-01-26 14:17:04 +0200

    get RequestParam from URL and display appropriate gif.

--- src/main/java/com/konped/controllers/GifController.java
+++ src/main/java/com/konped/controllers/GifController.java
@@ -1,15 +1,17 @@
 package com.konped.controllers;
 
+import com.konped.data.GifRepository;
 import com.konped.model.Gif;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.time.LocalDate;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GifController {
+
+  private GifRepository gifRepo = new GifRepository();
 
   @RequestMapping("/")
   public String listGifs() {
@@ -17,9 +19,8 @@ public class GifController {
   }
 
   @RequestMapping(value = "/gif", method = RequestMethod.GET)
-  public String gifDetails(ModelMap modelMap) {
-    Gif gif = new Gif("compiler-bot", LocalDate.of(1987, 4, 28),
-            "konstantinos", true );
+  public String gifDetails(@RequestParam("name") String name, ModelMap modelMap) {
+    Gif gif = gifRepo.findByName(name);
     modelMap.addAttribute(gif);
     return "gif-details";
   }
