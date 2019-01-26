b690667ffe41960f54d139b526e9f0ebabf38763
kpaidarakis <konstantinos.paidarakis@intrasoft-intl.com>
2019-01-26 19:19:15 +0200

    Add JAVA8Dialect for the format of the dates

--- src/main/java/com/konped/Application.java
+++ src/main/java/com/konped/Application.java
@@ -2,11 +2,22 @@ package com.konped;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+import org.thymeleaf.spring5.ISpringTemplateEngine;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 @SpringBootApplication
 public class Application {
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
+  }
+
+  private ISpringTemplateEngine templateEngine(ITemplateResolver templateResolver) {
+    SpringTemplateEngine engine = new SpringTemplateEngine();
+    engine.addDialect(new Java8TimeDialect());
+    engine.setTemplateResolver(templateResolver);
+    return engine;
   }
 }
